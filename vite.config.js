ec0cbd2f5372c8d2b7d25422af30ad8f65229b32
GonzalokHM <gonzalohm93@gmail.com>
2024-06-27 07:28:59 +0200

    fix target server

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
     proxy: {
       '/api': {
     // target desarrollo
-    target: 'https://fullback1.vercel.app',
+    target: 'https://fullbackevents.vercel.app',
     // target despliegue
         // target: 'http://localhost:4001',
         
